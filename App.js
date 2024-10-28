8fd00e1b0e778437eb99fcb4f20593136ff9cc7d
JonathanSedlakPSU <jms10330@psu.edu>
2024-10-28 10:36:59 -0400

    "Refactored Firebase configuration, added Firebase functions to App.js, removed FirebaseConfig.js, updated README.md, and updated package dependencies."

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+// Import Firebase functions
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2U4_yOGa1iLk9VA9fWVTzxRKDGbBX7Ec",
+  authDomain: "fir-project1-6db74.firebaseapp.com",
+  projectId: "fir-project1-6db74",
+  storageBucket: "fir-project1-6db74.appspot.com",
+  messagingSenderId: "137780388240",
+  appId: "1:137780388240:web:9013af8737ac39a3031315"
+};
+
+// Initialize Firebase and Firestore
+const FIREBASE_APP = initializeApp(firebaseConfig);
+const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+const FIREBASE_DB = getFirestore(FIREBASE_APP);
+
+// Function to add a new user document to Firestore
+async function addUser(userId, firstName, lastName, username) {
+  try {
+    await setDoc(doc(FIREBASE_DB, "Users", userId), {
+      FirstName: firstName,
+      LastName: lastName,
+      Username: username
+    });
+    console.log("User added!");
+  } catch (error) {
+    console.error("Error adding user: ", error);
+  }
+}
+///////////////////////////////////////////////////////////////////////
+// Main Function to start app
 
 export default function App() {
+  useEffect(() => {
+    // Call the addUser function here if you want it to run on mount
+    addUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Add User" onPress={addUser} />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 LightWeight is a mobile fitness app.
+
+#####
+
+To run the app with Expo:
+-yarn expo start
+or
+-npx expo start
+
+To clear cache:
+yarn expo start --clear
+or
+npx expo start --clear
+#####
