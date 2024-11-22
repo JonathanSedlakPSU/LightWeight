e30a913713181f53e1df0f098ff3d55a56ea24e8
JohnPham <89952683+jmp7651@users.noreply.github.com>
2024-11-22 15:20:51 -0500

    stylized header and nav bar

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Color, Border, FontSize, FontFamily } from "./GlobalStyles";
-
+import { TouchableOpacity } from "react-native-gesture-handler";
 // Import Firebase Configuration
 import { FIREBASE_AUTH, FIREBASE_DB, addUser } from "./FirebaseConfig";
 
@@ -15,30 +15,100 @@ import SignUpPage from "./Screens/SignUpPage";
 import HomePage from "./Screens/HomeScreen";
 import WorkoutsPage from "./Screens/WorkoutScreen";
 import ActivityPage from "./Screens/ActivityScreen";
-// Pop-Ups
-import GoalsPopUp from "./Screens/GoalsPop-Up";
-import DailyQuestPopup from "./Screens/DailyQuestPop-Up";
-import LogCaloriesPopUp from "./Screens/LogCaloriesPop-Up";
 
+// Import Icons
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 ///////////////////////////////////////////////////////////////////////
 
 // Create a TabBar at bottom of screen
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-function TabBar({route}) {
-
+function TabBar({ route }) {
   // Get the userId from the route in stack
   const { userId } = route.params;
 
   return (
-    
-    <Tab.Navigator initialRouteName="Profile">
-      <Tab.Screen name="Workouts" component={WorkoutsPage} />
-      <Tab.Screen name="Profile">
+    <Tab.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        tabBarStyle: { backgroundColor: Color.theme1 },
+        tabBarActiveTintColor: Color.colorWhite,
+        tabBarInactiveTintColor: Color.colorDarkgray,
+      }}
+    >
+      <Tab.Screen
+        name="Workouts"
+        component={WorkoutsPage}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="dumbbell" size={size} color={color} />
+          ),
+          headerStyle: {
+            backgroundColor: Color.theme1,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 10 }}
+            >
+              <FontAwesome name="bars" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={size} color={color} />
+          ),
+          headerStyle: {
+            backgroundColor: Color.theme1,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 10 }}
+            >
+              <FontAwesome name="bars" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      >
         {(props) => <HomePage {...props} userId={userId} />}
-      </Tab.Screen>    
-        <Tab.Screen name="Activity" component={ActivityPage} />
+      </Tab.Screen>
+      <Tab.Screen
+        name="Activity"
+        component={ActivityPage}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bar-chart-o" size={size} color={color} />
+            
+          ),
+          headerStyle: {
+            backgroundColor: Color.theme1,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 10 }}
+            >
+              <FontAwesome name="bars" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -49,10 +119,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginPage} />
           <Stack.Screen name="SignUp" component={SignUpPage} />
-          <Stack.Screen name="Home" component={TabBar} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="Home"
+            component={TabBar}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
